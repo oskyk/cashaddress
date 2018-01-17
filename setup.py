3d886947db4dfd569975ffd8bd622f53e2a8177e
Oskar Hladky <oskar.hladky@bitpanda.com>
2018-01-17 16:04:50 +0100

    version increase

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='cashaddress',
-      version='1.0.1',
+      version='1.0.2',
       packages=find_packages(),
       install_requires=['base58==0.2.5'],
       description='Python tool for converty bitcoin cash legacy addresses',
       author='Oskar Hladky',
       author_email='oskyks1@gmail.com',
       url='https://github.com/oskyk/cashaddress',
-      download_url='https://github.com/oskyk/cashaddress/archive/1.0.1.tar.gz',
+      download_url='https://github.com/oskyk/cashaddress/archive/1.0.2.tar.gz',
       python_requires='>=2.7',
       keywords=['bitcoincash', 'bch', 'address', 'cashaddress', 'legacy', 'convert']
 )
